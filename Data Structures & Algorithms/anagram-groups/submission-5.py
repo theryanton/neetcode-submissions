116990f7b5c9533758409c21211bf2469a5021bd
Ryan Ton <98431181+theryanton@users.noreply.github.com>
2026-09-22 19:30:08 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+        for i, string in enumerate(strs):
+            sorted_text = "".join(sorted(string))
+            if sorted_text in seen: # anagram exists
+                seen[sorted_text].append(string)
+            else: # new anagram
+                seen[sorted_text] = [string]
+        return [v for v in seen.values()]
+        

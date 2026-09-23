0a6fdf0ed8e7a33a20d2394e0810c7b7f792d279
Ryan Ton <98431181+theryanton@users.noreply.github.com>
2026-09-22 19:34:39 -0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = {}
+        for string in strs:
+            count = [0] * 26
+            for char in string:
+                count[ord(char) - ord('a')] += 1
+            if tuple(count) not in result: # new anagram
+                result[tuple(count)] = [string]
+            else: # combo already exists
+                result[tuple(count)].append(string)
+        return list(result.values())
